7696fa8e32d023bb578c8bc81e35782cd4f2a039
fbizza <francesco.bizzarri@stud.unifi.it>
2023-02-09 19:04:26 +0100

    Compose only squares and not rectangles and change the display window

--- src/AlphaComposition.py
+++ src/AlphaComposition.py
@@ -7,28 +7,35 @@ def alpha_compose(img, num_squares, alpha):
     for i in range(num_squares):
         x1 = np.random.randint(0, width)
         y1 = np.random.randint(0, height)
-        x2 = np.random.randint(x1, width)
-        y2 = np.random.randint(y1, height)
-        square = np.ones((y2-y1, x2-x1, channels), dtype=np.float32) * 0
+        size = np.random.randint(10, min(width//3, height//3))
+        x2 = x1 + size
+        y2 = y1 + size
+        if x2 > width:
+            x2 = width
+            x1 = x2 - size
+        if y2 > height:
+            y2 = height
+            y1 = y2 - size
+        square = np.ones((size, size, channels), dtype=np.float32) * 0
+        square = cv2.resize(square, (y2-y1, x2-x1), interpolation = cv2.INTER_NEAREST)
         img_copy[y1:y2, x1:x2] = square * alpha + img_copy[y1:y2, x1:x2] * (1 - alpha)
     return img_copy
+
 
 if __name__ == '__main__':
     image_path = "../MyImage.jpg"
     img = cv2.imread(image_path)
     num_squares = 5
-    alpha = 0.7
+    alpha = 0.6
     augmented_img = alpha_compose(img, num_squares, alpha)
-    window_name = "Original Image"
-    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-    cv2.imshow(window_name, img)
-    cv2.resizeWindow(window_name, img.shape[1]//2, img.shape[0]//2)
-    window_name = "Alpha Composed Image"
-    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-    cv2.imshow(window_name, augmented_img)
-    cv2.resizeWindow(window_name, augmented_img.shape[1]//2, augmented_img.shape[0]//2)
+
+    both_images = np.hstack((img, augmented_img))
+    cv2.namedWindow("Both Images", cv2.WINDOW_NORMAL)
+    cv2.imshow("Both Images", both_images)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
 
 
 
