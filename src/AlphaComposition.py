a222364f39d740a6d0985bb70cd9e99647c3f57c
fbizza <francesco.bizzarri@stud.unifi.it>
2023-02-10 15:26:53 +0100

    Handle directory of multiple images

--- src/AlphaComposition.py
+++ src/AlphaComposition.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import os
 
 def alpha_compose(img, num_squares, alpha):
     height, width, channels = img.shape
@@ -7,7 +8,7 @@ def alpha_compose(img, num_squares, alpha):
     for i in range(num_squares):
         x1 = np.random.randint(0, width)
         y1 = np.random.randint(0, height)
-        size = np.random.randint(10, min(width//3, height//3))
+        size = width//7
         x2 = x1 + size
         y2 = y1 + size
         if x2 > width:
@@ -17,23 +18,30 @@ def alpha_compose(img, num_squares, alpha):
             y2 = height
             y1 = y2 - size
         square = np.ones((size, size, channels), dtype=np.float32) * 0
-        square = cv2.resize(square, (y2-y1, x2-x1), interpolation = cv2.INTER_NEAREST)
         img_copy[y1:y2, x1:x2] = square * alpha + img_copy[y1:y2, x1:x2] * (1 - alpha)
     return img_copy
 
 
 if __name__ == '__main__':
-    image_path = "../MyImage.jpg"
-    img = cv2.imread(image_path)
-    num_squares = 5
+    dir_path = "../Images"
+    num_squares = 50
     alpha = 0.6
-    augmented_img = alpha_compose(img, num_squares, alpha)
+    processed_images = []
 
-    both_images = np.hstack((img, augmented_img))
-    cv2.namedWindow("Both Images", cv2.WINDOW_NORMAL)
-    cv2.imshow("Both Images", both_images)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # Loop to read and process the images:
+    for filename in os.listdir(dir_path):
+        image_path = os.path.join(dir_path, filename)
+        img = cv2.imread(image_path)
+        augmented_img = alpha_compose(img, num_squares, alpha)
+        processed_images.append((img, augmented_img))
+
+    #Loop to display the images:
+    for img, augmented_img in processed_images:
+        both_images = np.hstack((img, augmented_img))
+        cv2.namedWindow("Both Images", cv2.WINDOW_NORMAL)
+        cv2.imshow("Both Images", both_images)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
 
